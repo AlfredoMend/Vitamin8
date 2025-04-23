029fce67319a34ccce0983d2bceb6f77571e4f7c
AlfredoMend <fredo@berkeley.edu>
2025-04-22 18:05:39 -0700

    finished

---
+++ my-app/app/dark-mode/page.js
@@ -0,0 +1,12 @@
+// app/dark-mode/page.js
+import Home from '../page';
+
+export default function DarkModePage() {
+  return <Home />;
+}
+// app/light-mode/page.js
+import Home from '../page';
+
+export default function LightModePage() {
+  return <Home />;
+}
